f400fe739a111e53fc48eb531d94351b6e9f4e48
Jim Counts <jcounts18@gmail.com>
2016-11-29 13:46:32 -0800

    Fix path to state

--- scripts/terraform2/ecr.update-image.sh
+++ scripts/terraform2/ecr.update-image.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-terraform output -state=./scripts/terraform2/terraform.tfstate update-image-script | bash
+terraform output -state=./scripts/terraform2/.terraform/terraform.tfstate update-image-script | bash
